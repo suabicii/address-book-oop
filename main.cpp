7ee205c45aeaaa23bac07b6eec80f11fe453ebbe
Suabicii <michal.slabik3@wp.pl>
2020-02-20 12:23:52 +0100

    Adding main menu

--- main.cpp
+++ main.cpp
@@ -7,10 +7,36 @@ using namespace std;
 int main()
 {
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt");
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
-    ksiazkaAdresowa.rejestracjaUzytkownika();
-    ksiazkaAdresowa.rejestracjaUzytkownika();
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    char wybor;
+
+    while (1)
+    {
+        system("cls");
+        cout << "    >>> MENU  GLOWNE <<<" << endl;
+        cout << "---------------------------" << endl;
+        cout << "1. Rejestracja" << endl;
+        cout << "2. Logowanie" << endl;
+        cout << "3. Koniec programu" << endl;
+        cout << "---------------------------" << endl;
+        cout << "Twoj wybor: ";
+        cin >> wybor;
+
+        switch (wybor)
+        {
+        case '1':
+            ksiazkaAdresowa.rejestracjaUzytkownika();
+            break;
+        case '2':
+            break;
+        case '3':
+            exit(0);
+            break;
+        default:
+            cout << "Nie ma takiej opcji w menu!" << endl;
+            Sleep(1500);
+            break;
+        }
+    }
 
     return 0;
 }
