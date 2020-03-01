b399d184583560b895f8852b89bd2f6f509f064d
Suabicii <michal.slabik3@wp.pl>
2020-03-01 18:42:30 +0100

    Added auxiliary methods for: loading lines, loading chars, loading integers

--- MetodyPomocnicze.cpp
+++ MetodyPomocnicze.cpp
@@ -18,3 +18,46 @@ string MetodyPomocnicze::pobierzLiczbe(string tekst, int pozycjaZnaku)
     }
     return liczba;
 }
+
+char MetodyPomocnicze::wczytajZnak()
+{
+    string wejscie = "";
+    char znak = {0};
+
+    while (true)
+    {
+        getline(cin, wejscie);
+
+        if (wejscie.length() == 1)
+        {
+            znak = wejscie[0];
+            break;
+        }
+        cout << "To nie jest pojedynczy znak. Wpisz ponownie." << endl;
+    }
+    return znak;
+}
+
+int MetodyPomocnicze::wczytajLiczbeCalkowita()
+{
+    string wejscie = "";
+    int liczba = 0;
+
+    while (true)
+    {
+        getline(cin, wejscie);
+
+        stringstream myStream(wejscie);
+        if (myStream >> liczba)
+            break;
+        cout << "To nie jest liczba. Wpisz ponownie. " << endl;
+    }
+    return liczba;
+}
+
+string MetodyPomocnicze::wczytajLinie()
+{
+    string wejscie = "";
+    getline(cin, wejscie);
+    return wejscie;
+}
--- MetodyPomocnicze.h
+++ MetodyPomocnicze.h
@@ -11,6 +11,9 @@ class MetodyPomocnicze
 public:
     static string konwersjaIntNaString(int liczba);
     static string pobierzLiczbe(string tekst, int pozycjaZnaku);
+    static char wczytajZnak();
+    static int wczytajLiczbeCalkowita();
+    static string wczytajLinie();
 };
 
 #endif
