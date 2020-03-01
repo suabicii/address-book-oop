6e662c422aa0eafa6bcded58fb67ca2f4a9935f5
Suabicii <michal.slabik3@wp.pl>
2020-03-01 21:26:41 +0100

    Refactored adding users and addressees to text files

--- PlikZAdresatami.cpp
+++ PlikZAdresatami.cpp
@@ -31,9 +31,9 @@ bool PlikZAdresatami::czyPlikJestPusty()
     fstream plikTekstowy;
     plikTekstowy.seekg(0, ios::end);
     if (plikTekstowy.tellg() == 0)
-        return true;
-    else
         return false;
+    else
+        return true;
 }
 
 string PlikZAdresatami::zamienDaneAdresataNaLinieZDanymiOddzielonymiPionowymiKreskami(Adresat adresat)
--- PlikZUzytkownikami.cpp
+++ PlikZUzytkownikami.cpp
@@ -62,9 +62,9 @@ bool PlikZUzytkownikami::czyPlikJestPusty()
     fstream plikTekstowy;
     plikTekstowy.seekg(0, ios::end);
     if (plikTekstowy.tellg() == 0)
-        return true;
-    else
         return false;
+    else
+        return true;
 }
 
 string PlikZUzytkownikami::zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik)
